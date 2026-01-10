d5318396ecc1cea44b91ae5759fa67258c4795bc
Elmar Birembekov <birembekove@gmail.com>
2026-01-10 12:20:34 +0300

    First coomit

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Elmar006/api-backend/database"
+	"github.com/Elmar006/api-backend/handlers"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	database.ConnectDB()
+	r := chi.NewRouter()
+
+	r.Get("/tasks", handlers.GetTasks)
+	r.Post("/tasks", handlers.PostTask)
+	r.Get("/task/{id}", handlers.GetTaskByID)
+	r.Delete("/task/{id}", handlers.DeleteTask)
+
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		fmt.Printf("Error start serve: %v", err)
+		return
+	}
+}
